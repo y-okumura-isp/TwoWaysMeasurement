1c7993f6814365fa628dce8a3acd2cd4e350af28
y-okumura <y-okumura@isp.co.jp>
2020-03-10 18:45:14 +0900

    Refactor 1exec_1node by TwoWaysNode

--- rclcpp/1exec/main_1exec_1node_pubsub.cpp
+++ rclcpp/1exec/main_1exec_1node_pubsub.cpp
@@ -5,79 +5,22 @@
 #include <numeric>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "../common/TwoWaysNode.hpp"
 #include "../common/tw_utils.hpp"
 
-using namespace std::chrono_literals;
-
-using _SC = std::chrono::steady_clock;
-using TIME_POINT = std::chrono::time_point<std::chrono::steady_clock>;
-
 const char * node_name = "one_node_pub_sub";
-const char * namespace_ = "ns";
-const char * topic_name = "ping";
-const int qos = 10;
-const int period_ns = 100 * 1000 * 1000; // wake up period[ns]
-const int num_bin = 10000; // number of time reports.
 
-class PubSubNode : public rclcpp::Node
+class PubSubNode : public TwoWaysNode
 {
 public:
-  PubSubNode(const rclcpp::NodeOptions & options)
-      : Node(node_name, options), count(0)
+  PubSubNode(
+      const TwoWaysNodeOptions & tw_options,
+      const rclcpp::NodeOptions & options)
+      : TwoWaysNode(node_name, tw_options.namespace_, tw_options, options)
   {
-    // sub
-    auto callback_sub =
-        [this](const std_msgs::msg::String::SharedPtr msg) -> void
-        {
-          (void)msg;
-
-          auto now = _SC::now();
-          auto diff = std::chrono::duration_cast<std::chrono::nanoseconds>(now - this->time_send);
-          this->recv_jitters[this->count % num_bin] = diff.count();
-        };
-    sub_ = this->create_subscription<std_msgs::msg::String>(topic_name, qos, callback_sub);
-
-    // pub
-    pub_ = this->create_publisher<std_msgs::msg::String>(topic_name, qos);
-    auto callback_pub =
-        [this]() -> void
-        {
-          // calc timer jitter
-          TIME_POINT now = _SC::now();
-          auto expect = this->epoch + std::chrono::nanoseconds(period_ns) * (this->count + 1);
-          auto diff = std::chrono::duration_cast<std::chrono::nanoseconds>(now - expect);
-          // std::cout << "timer latency: " << diff.count() << "[ns]" << std::endl;;
-          this->wakeup_jitters[this->count % num_bin] = diff.count();
-
-          // pub
-          std_msgs::msg::String msg;
-          msg.data = "HelloWorld" + std::to_string(this->count);
-          // std::cout << "send: " < msg.data.c_str() << std::endl;
-          this->count++;
-          this->time_send = _SC::now();
-          pub_->publish(msg);
-        };
-
-    // set timer
-    this->epoch = _SC::now();
-    timer = this->create_wall_timer(std::chrono::nanoseconds(period_ns), callback_pub);
+    this->setup_ping_publisher();
+    this->setup_ping_subscriber();
   }
-
-  void print_result() const;
-
-  int count;
-
-  TIME_POINT epoch;
-  int wakeup_jitters[num_bin];
-  int recv_jitters[num_bin];
-
-private:
-  TIME_POINT time_send;
-
-  rclcpp::TimerBase::SharedPtr timer;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub_;
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_;
 };
 
 int main(int argc, char *argv[])
@@ -87,17 +30,19 @@ int main(int argc, char *argv[])
   rclcpp::init(argc, argv);
   rclcpp::executors::SingleThreadedExecutor exec;
 
+  TwoWaysNodeOptions tw_options;
   rclcpp::NodeOptions node_options;
   // TODO: node_options.allocator = ...;
   // TODO: node_options.use_intra_process_comms = ...;
-  auto n = std::make_shared<PubSubNode>(node_options);
+  auto n = std::make_shared<PubSubNode>(tw_options, node_options);
 
   exec.add_node(n);
   exec.spin();
   exec.remove_node(n);
 
-  print_result("wakeup_jitters", n->wakeup_jitters, n->count, num_bin);
-  print_result("recv_jitters",   n->recv_jitters,   n->count, num_bin);
+  auto num_bin = tw_options.num_bin;
+  print_result("wakeup_jitters", n->ping_wakeup_jitters_, n->ping_pub_count_, num_bin);
+  print_result("recv_jitters",   n->ping_sub_jitters_,    n->ping_sub_count_, num_bin);
 
   rclcpp::shutdown();
 }
