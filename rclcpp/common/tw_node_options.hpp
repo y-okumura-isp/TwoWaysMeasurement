f67badcc0e90c72250e9bd726d1e9cd26b22fedf
y-okumura <y-okumura@isp.co.jp>
2020-03-12 16:36:32 +0900

    Change initialization style

--- rclcpp/common/tw_node_options.hpp
+++ rclcpp/common/tw_node_options.hpp
@@ -13,15 +13,6 @@ struct JitterReportOptions
 class TwoWaysNodeOptions {
 public:
   TwoWaysNodeOptions()
-      : node_name_pub("one_node_sub"),
-        node_name_sub("one_node_pub"),
-        namespace_("ns"),
-        topic_name("ping"),
-        qos(10),
-        period_ns(10 * 1000 * 1000),
-        use_tlsf_allocator(true),
-        use_message_pool_memory_strategy(true),
-        use_intra_process_comms(false)
   {
     ping_wakeup.bin = 600;
     ping_wakeup.round_ns = 1000;
@@ -36,25 +27,25 @@ public:
   /// Get Executor
   rclcpp::executor::Executor::SharedPtr get_executor();
 
-  const char * node_name_pub;
-  const char * node_name_sub;
-  const char * namespace_;
-  const char * topic_name;
+  const char * node_name_pub = "node_pub";
+  const char * node_name_sub = "node_sub";
+  const char * namespace_ = "ns";
+  const char * topic_name = "ping";
   rclcpp::QoS qos = rclcpp::QoS(1).best_effort();
 
   // wake up period[ns]
-  const int period_ns;
+  const int period_ns = 10 * 1000 * 1000;
   JitterReportOptions ping_wakeup;
   JitterReportOptions ping_sub;
 
   // Options for rclcpp::Executor
-  const bool use_tlsf_allocator;
+  const bool use_tlsf_allocator = true;
 
   // Options for rclcpp::Subscription
-  const bool use_message_pool_memory_strategy;
+  const bool use_message_pool_memory_strategy = true;
 
   // Options for rclcpp::Node
-  const bool use_intra_process_comms;
+  const bool use_intra_process_comms = false;
 };
 
 #endif  /* SETTING_H_ */
