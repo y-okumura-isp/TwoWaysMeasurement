f39de4aef88bcbcbae667542a29ba8b7f3bef659
y-okumura <y-okumura@isp.co.jp>
2020-06-29 13:03:18 +0900

    Use TwoWaysNode directly to unify main function

--- rclcpp/1exec/main_1exec_1node_ping_pong.cpp
+++ rclcpp/1exec/main_1exec_1node_ping_pong.cpp
@@ -10,20 +10,6 @@
 
 const char * node_name = "one_node_ping_pong";
 
-class PubSubNode : public TwoWaysNode
-{
-public:
-  PubSubNode(
-      const TwoWaysNodeOptions & tw_options,
-      const rclcpp::NodeOptions & options)
-      : TwoWaysNode(node_name, tw_options.namespace_, tw_options, options)
-  {
-    this->setup_ping_publisher();
-    this->setup_ping_subscriber(true);
-    this->setup_pong_subscriber();
-  }
-};
-
 int main(int argc, char *argv[])
 {
   std::cout << "Press C-c to quit" << std::endl;
@@ -35,7 +21,10 @@ int main(int argc, char *argv[])
   auto exec = tw_options.get_executor();
   rclcpp::NodeOptions node_options;
   tw_options.set_node_options(node_options);
-  auto n = std::make_shared<PubSubNode>(tw_options, node_options);
+  auto n = std::make_shared<TwoWaysNode>("node_1e1n", "ns", tw_options, node_options);
+  n->setup_ping_publisher();
+  n->setup_ping_subscriber(true);
+  n->setup_pong_subscriber();
 
   exec->add_node(n);
   SET_REALTIME_SETTING_RRTS(tw_options);
