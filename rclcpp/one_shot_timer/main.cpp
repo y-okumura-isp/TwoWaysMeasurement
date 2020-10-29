45568ec294fc38abfd419510c1018867995c4c01
y-okumura <y-okumura@isp.co.jp>
2020-10-29 15:59:52 +0900

    Fix build error of one_shot_timer

--- rclcpp/one_shot_timer/main.cpp
+++ rclcpp/one_shot_timer/main.cpp
@@ -5,7 +5,16 @@ int main(int argc, char *argv[])
   std::cout << "Press C-c to quit" << std::endl;
 
   TwoWaysNodeOptions tw_options(argc, argv);
-  SET_REALTIME_SETTING_RRRR(tw_options);
+
+  if(lock_and_prefault_dynamic(tw_options.prefault_dynamic_size) < 0) {
+    std::cerr << "lock_and_prefault_dynamic failed" << std::endl;
+  }
+
+  // setup child process scheule
+  size_t priority = 0;
+  int policy = 0;
+  tw_options.get_child_thread_policy(priority, policy);
+  set_sched_priority("child", priority, policy);
 
   rclcpp::init(argc, argv);
   auto exec = tw_options.get_executor();
@@ -16,7 +25,10 @@ int main(int argc, char *argv[])
   n->setup_periodic_trigger();
 
   exec->add_node(n);
-  SET_REALTIME_SETTING_RRTS(tw_options);
+
+  tw_options.get_main_thread_policy(priority, policy);
+  set_sched_priority("main", priority, policy);
+
   exec->spin();
   exec->remove_node(n);
 
